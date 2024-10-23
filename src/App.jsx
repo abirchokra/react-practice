9d8938a4f79d601d14328aa2e35a07b8c10d3a01
abirchokra <dipalokch@gmail.com>
2024-10-23 21:59:33 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Blog from './components/Blogs/Blogs'
+import Header from './components/header/Header'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <Header></Header>
+      <Blogs></Blogs>
+     
+      
+      
+    </>
+  )
+}
+
+export default App
